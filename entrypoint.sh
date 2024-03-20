4cc39b5648cb480fe97dace5bce5923a914627e8
Talha Akber <talha_akber@hotmail.com>
2024-03-20 05:13:06 +0100

    test: adding unit test for the endpoint count

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # Schema Update
-php bin/console doctrine:schema:update --force
+php bin/console doctrine:schema:update --force --no-interaction
+
+# Creating Test Database
+php bin/console --env=test doctrine:database:create --if-not-exists
+
+# Creating Schema in Test Database
+php bin/console --env=test doctrine:schema:update --force --no-interaction
+
+# Seeding with Fixture Data
+php bin/console --env=test doctrine:fixtures:load --no-interaction
 
 # Start the Symfony server
 service cron start && symfony server:start --no-tls
